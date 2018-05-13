c886e6cd269f42dbc47db5890c0ed05dc50faf44
g-arais <gustavo@unorobotica.com.br>
2018-05-13 20:40:01 -0300

    Add files via upload

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -14,6 +14,7 @@ import android.graphics.Color;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import android.app.Notification;
 /**
  * Created by Felipe Echanique on 08/06/2016.
  */
